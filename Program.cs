973c57acbb3d32fcb14d6ff9b14d9a64f6e6a15b
PriyaIhsan <60973417+priyaihsan@users.noreply.github.com>
2022-01-11 11:40:53 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using WCFService_2Way_050;
+
+namespace ServerChatApp_050_PriyaIhsan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObject = null;
+            try
+            {
+                hostObject = new ServiceHost(typeof(ServiceCallback));
+                hostObject.Open();
+
+                Console.WriteLine("Server On, Ready to use");
+                Console.ReadLine();
+
+                hostObject.Close();
+            }catch(Exception ex)
+            {
+                hostObject = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
